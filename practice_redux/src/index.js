acce23a7c40a52dae1a74b8c77503d91165876ea
sangwon0724 <leews0724@naver.com>
2021-06-15 15:23:49 +0900

    3.1 Connecting the Store

--- practice_redux/src/index.js
+++ practice_redux/src/index.js
@@ -2,15 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
-import reportWebVitals from './reportWebVitals';
+import { Provider } from "react-redux";
+import store from "./store";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
-reportWebVitals();
 
 
 
